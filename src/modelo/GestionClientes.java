4b7a86f8648ccfa280b5a6eaa34fd0b6f1c699cd
herre <javier.herreria23@estudiantes.uva.es>
2024-12-31 00:43:04 +0100

    Test commit

--- src/modelo/GestionClientes.java
+++ src/modelo/GestionClientes.java
@@ -1,8 +1,35 @@
 package modelo;
 
 import ES.MyInput;
+import menus.Menu;
+
 
 
 public class GestionClientes {
+
+    private Concesionario c;
+
+    public GestionClientes(Concesionario c){
+        this.c = c;
+    }
+
+    public void showMenu(){
+
+        //if (this.c.sizeCliente)
+        Menu menuClientes = new Menu("Menú Clientes",
+                new String[]{"Añadir Cliente",
+                        "Información de cliente",
+                        "Información de todos los clientes",
+                        "Información de clientes que desean recibir publicidad"});
+
+        int opcion;
+        Cliente cl;
+
+    }
+
+
+
+
+
 
 }
