108d8605503378161e48558536b1f639e697ab9b
herre <javier.herreria23@estudiantes.uva.es>
2024-12-31 02:28:26 +0100

    ampliacion de codigo

--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -4,13 +4,22 @@ import java.io.Serializable;
 
 public class Cliente implements Serializable {
 
+    private static final long serialVersionUID = 1L;
     private String DNI;
     private String nombre;
     private String apellidos;
     private String telefono;
     private boolean deseaInfo;
 
-    public Cliente(String DNI, String nombre, String apellidos){
+    public Cliente(){
+        this.DNI = "";
+        this.nombre = "";
+        this.apellidos = "";
+        this.telefono = "";
+        this.deseaInfo = false;
+    }
+
+    public Cliente( String DNI, String nombre, String apellidos ){
         this.DNI = DNI;
         this.nombre = nombre;
         this.apellidos = apellidos;
@@ -18,7 +27,7 @@ public class Cliente implements Serializable {
         this.deseaInfo = false;
     }
 
-    public Cliente(String DNI, String nombre, String apellidos, String telefono, boolean deseaInfo){
+    public Cliente( String DNI, String nombre, String apellidos, String telefono, boolean deseaInfo ){
         this.DNI = DNI;
         this.nombre = nombre;
         this.apellidos = apellidos;
@@ -26,29 +35,45 @@ public class Cliente implements Serializable {
         this.deseaInfo = deseaInfo;
     }
 
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
     }
 
     public String getTelefono() {
         return telefono;
     }
 
-    public void setDeseaInfo(Boolean deseaInfo){
-        this.deseaInfo = deseaInfo;
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
     }
 
-    public boolean getDeseaInfo() {
+    public boolean isDeseaInfo() {
         return deseaInfo;
+    }
+
+    public void setDeseaInfo(boolean deseaInfo) {
+        this.deseaInfo = deseaInfo;
     }
 
 
 }
-
-
-
-
-
-
-
-
--- src/modelo/GestionClientes.java
+++ src/modelo/GestionClientes.java
@@ -13,10 +13,27 @@ public class GestionClientes {
         this.c = c;
     }
 
-    public void showMenu(){
+    public void showMenu() {
 
-        //if (this.c.sizeCliente)
-        Menu menuClientes = new Menu("Menú Clientes",
+        // * FALTA IMPLEMENTAR sizeCliente EN CONCESIONARIO *
+        /*
+        if (this.c.sizeCliente() == 0){
+            System.out.println("ERROR: No hay clientes disponibles.");
+            if(MyInput.yesNoQuestion("¿Desea dar de alta un cliente? [Y/N]")){
+                this.GestionClientes.altaCliente();
+            }
+        }
+        */
+
+        /*
+        if (this.c.sizeCliente() == 0){
+            System.out.println("ERROR: No hay clientes disponibles.);
+            return;
+        }
+        */
+
+
+        Menu menu_clientes = new Menu("Menú Clientes",
                 new String[]{"Añadir Cliente",
                         "Información de cliente",
                         "Información de todos los clientes",
@@ -25,11 +42,58 @@ public class GestionClientes {
         int opcion;
         Cliente cl;
 
+        do{
+            opcion = menu_clientes.show();
+            switch(opcion){
+                case 1:
+                    altaCliente();
+                    break;
+                case 2:
+                    infoCliente();
+                    break;
+                case 3:
+                    infoTodosClientes();
+                    break;
+                case 4:
+                    infoClientesPublicidad();
+                    break;
+                default:
+
+            }
+        }while(opcion!=0);
     }
 
+    // Implementar este código en Concesionario, e implementar para tener los arraylist de clientes públicos, como los de secciones y vehículos.
 
+    /*public Cliente buscarPorDNI(String dni) {
+        for (Cliente cliente : clientes) { // Recorrer la lista de clientes
+            if (cliente.getDNI().equals(dni)) { // Compara el DNI
+                return cliente; // Devuelve el cliente si encuentra uno con el mismo DNI
+            }
+        }
+        return null; // Si no encuentra un cliente con ese DNI, devuelve null
+    }
+    */
 
+    public void altaCliente(){
+        Cliente cliente = new Cliente();
 
+        System.out.println("========================================================");
+        System.out.println("Alta de un cliente");
+        System.out.println("--------------------------------------------------------");
+        cliente.setDNI( MyInput.readString("DNI: "));
+        cliente.setNombre(MyInput.readString("Nombre: "));
+        cliente.setApellidos(MyInput.readString("Apellidos: "));
+        cliente.setTelefono(MyInput.readString("Número de teléfono: "));
+        cliente.setDeseaInfo(MyInput.yesNoQuestion("¿Desea recibir notificaciones publicitarias? [Y/N]"));
+        // *FALTA IMPLEMENTAR add.Cliente() EN CONCESIONARIO*
+        // c.addCliente(cliente);
+    }
+
+    // Implementar buscador de cliente por DNI, si existe, devuelve el cliente, y si no existe, devuelve null;
+    public void infoCLiente(){
+
+    }
 
 
 }
